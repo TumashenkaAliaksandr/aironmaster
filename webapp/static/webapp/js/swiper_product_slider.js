ff02edd79084ae17bcc9d5c6240cbe1ef5256aa8
Admin <tumashenkaaliaksandr@gmail.com>
2025-07-28 09:54:56 +0300

    added new styles and change sweepr slider on the owl carusel

--- webapp/static/webapp/js/swiper_product_slider.js
+++ webapp/static/webapp/js/swiper_product_slider.js
@@ -1,32 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.second-product-slider', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop: true,
-
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: false,
-        },
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-
-        freeMode: false,
-
-        breakpoints: {
-            320: {slidesPerView: 1, spaceBetween: 10},
-            640: {slidesPerView: 2, spaceBetween: 15},
-            768: {slidesPerView: 3, spaceBetween: 15},
-            1024: {slidesPerView: 4, spaceBetween: 20},
-        },
+$(document).ready(function() {
+    $('.second-product-slider').owlCarousel({
+        loop: true,                   // Бесконечный цикл
+        nav: true,                    // Стрелки вперед/назад
+        dots: true,                   // Пагинация (точки)
+        autoplay: true,               // Автозапуск
+        autoplayTimeout: 3000,        // Интервал прокрутки в мс
+        autoplayHoverPause: false,    // Не останавливать при наведении
+        margin: 20,                   // Отступ между слайдами
+        navText: ['<span class="arrow-left">&#10094;</span>', '<span class="arrow-right">&#10095;</span>'], // Можно свои стрелки, или иконки
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10
+            },
+            640: {
+                items: 2,
+                margin: 15
+            },
+            768: {
+                items: 3,
+                margin: 15
+            },
+            1024: {
+                items: 4,
+                margin: 20
+            }
+        }
     });
 });
